3dbceac205886d72300f035f3b522573c5af5789
Max <maxime.vollat@epitech.eu>
2021-05-15 19:33:15 +0200

    [ADD] octopus tentacle

--- Player/Octopus/TentacularMayhem.gd
+++ Player/Octopus/TentacularMayhem.gd
@@ -1,11 +1,14 @@
 extends Node
 
-const cd = 5000
-const fire_rate = 300
+const cd = 8000
+const fire_rate = 100
 var use_time = 0
 var shoot_time = 0
-var active_time = 1200
+var active_time = 3500
+var alternate_speed = 750
+var last_alternate = 0
 var active_skill = false
+var pos_spawn = Vector2.ZERO
 
 onready var load_path = "res://Prefabs/Characters/Projectile.tscn"
 
@@ -13,10 +16,11 @@ var time_last_used = 0
 var actual_time = 0
 onready var rotater = $Rotater
 
-const rotate_speed = 120
-const base_rotation = 0
-const spawn_point_count = 8
-const radius = 100
+var base_rotate_speed = 50
+var rotate_speed = 0
+var rotate_dir = 0
+const spawn_point_count = 6
+const radius = 500
 
 var skill_manager
 var player
@@ -27,7 +31,6 @@ func _ready():
 	skill_manager = get_parent().get_parent()
 	player = skill_manager.get_parent()
 	var step = 2 * PI / spawn_point_count
-	
 	for i in range(spawn_point_count):
 		var spawn_point = Node2D.new()
 		var pos = Vector2(radius, 0).rotated(step * i)
@@ -47,9 +50,10 @@ func skill():
 		getWorld().add_child(bullet)
 #		bullet.duplicate(true)
 		bullet.position = player.position
+		bullet.distance_max = radius
 		bullet.player = player.name
 		bullet.size = 0.75
-		bullet.speed = 100
+		bullet.speed = base_rotate_speed * 3
 		bullet.set_values()
 		bullet.move_dir = s.global_rotation	
 
@@ -57,11 +61,13 @@ func _process(delta):
 	if (!active_skill):
 		return
 	actual_time = OS.get_ticks_msec()
+	## rotate_speed = base_rotate_speed + ((((rotate_max - base_rotate_speed) * (actual_time - time_last_used)) / float(active_time)))
 	var new_rotation = rotater.rotation_degrees + rotate_speed * delta
 	rotater.rotation_degrees = fmod(new_rotation, 360)
 	if (actual_time - use_time >= active_time):
 		active_skill = false
 		return
+	## max_fire_rate + fire_rate - (fire_rate * (rotate_speed / rotate_max * 2))
 	if (actual_time - shoot_time >= fire_rate):
 		skill()
 		shoot_time = actual_time
@@ -69,8 +75,15 @@ func _process(delta):
 func useSkill():
 	actual_time = OS.get_ticks_msec()
 	if (actual_time - time_last_used < cd && time_last_used > 0):
+		if (actual_time - last_alternate >= alternate_speed):
+			last_alternate = actual_time
+			rotate_speed *= -1
 		return
+	last_alternate = actual_time
 	time_last_used = actual_time
+	rotate_speed = base_rotate_speed
+	rotate_dir = 1
 	use_time = actual_time
 	shoot_time = 0
 	active_skill = true
+	pos_spawn = player.position
---
+++ Scenes/InGame/UserInterface/UIPopup.gd
@@ -0,0 +1,32 @@
+extends Popup
+
+onready var firstChild = $VerticalAlign.get_child(0)
+onready var lastFocus = firstChild
+
+func _input(event):
+	if Input.is_action_just_released("pause"):
+		get_tree().paused = !get_tree().paused
+		visible = !visible
+		if visible == true:
+			firstChild.grab_focus()
+		
+
+
+func _ready():
+	var btns = $VerticalAlign.get_children()
+	
+	get_tree().get_root().connect("size_changed", self, "resize")
+	for b in btns:
+		b.connect("mouse_entered", self, "selfFocus", [b])
+		b.connect("focus_entered", self, "selfFocus", [b])
+	
+func resize():
+	visible = !visible
+	visible = !visible
+	
+func selfFocus(b):
+	lastFocus = b
+	lastFocus.grab_focus()
+	
+func reFocus():
+	lastFocus.grab_focus()
