a8b6777d9a5c1ef9569891b81be0abd863c19589
alexandre <alexandre.dao@epitech.eu>
2021-05-21 22:17:25 +0900

    Add Join, host button and option scene

---
+++ Scenes/ModeSelection/ControllerFocus.gd
@@ -0,0 +1,12 @@
+extends VBoxContainer
+
+func _ready():
+	var btns = get_tree().get_nodes_in_group("Button")
+	
+	btns[0].grab_focus()
+	for btn in btns:
+		btn.connect("mouse_entered", self, "force_focus", [btn])
+		btn.connect("focus_entered", self, "force_focus", [btn])
+
+func force_focus(btn):
+	btn.grab_focus()
